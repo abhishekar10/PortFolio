37064772a41626472fb8b847ba6a61986daceaf6
Abhishek A R <getarabhishek@gmail.com>
2024-11-25 13:45:58 +0530

    Add theme buttons and full fledged dark theme

--- JS/script.js
+++ JS/script.js
@@ -3,13 +3,10 @@ body = document.querySelector("#body");
 navHolder = document.querySelector("#navHolder");
 navBar = document.querySelector("#navBar");
 navBarTitles = document.getElementsByTagName("a");
-headingOne = document.querySelectorAll("#text")
+themeIcons = document.getElementsByTagName("i");
+headingOne = document.querySelectorAll("#text");
 themeButton.addEventListener("click",() => {
     if(navBarTitles[0].classList.contains("a-light")) {
-        for(let i = 0; i <headingOne.length; i++) {
-            headingOne[i].classList.add("text-color-black");
-            headingOne[i].classList.remove("text-color");
-        }
         body.classList.add("body-black");
         body.classList.remove("body");
         navBar.classList.add("nav-bar-black");
@@ -20,6 +17,13 @@ themeButton.addEventListener("click",() => {
             navBarTitles[i].classList.add("a-dark");
             navBarTitles[i].classList.remove("a-light")
         }
+        for(let i = 0; i <headingOne.length; i++) {
+            headingOne[i].classList.add("text-color-black");
+            headingOne[i].classList.remove("text-color");
+        }
+        themeIcons[0].classList.add("hide");
+        themeIcons[1].classList.remove("hide");
+        themeButton.classList.add("theme-setter-black");
     } else {        
         for(let i = 0; i <headingOne.length; i++) {
             headingOne[i].classList.remove("text-color-black");
@@ -35,6 +39,9 @@ themeButton.addEventListener("click",() => {
             navBarTitles[i].classList.remove("a-dark");
             navBarTitles[i].classList.add("a-light")
         }
+        themeIcons[0].classList.remove("hide");
+        themeIcons[1].classList.add("hide");
+        themeButton.classList.remove("theme-setter-black");
     }
 
 })
